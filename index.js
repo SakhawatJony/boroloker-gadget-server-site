099ec26f058e60d610b8e1ae9ee4cbeb8244a95e
Sakhawat Hosen <sakhawathosen320@gmail.com>
2023-05-29 00:37:26 +0600

    serversite connection and data products get

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
-const express = require('express');
+const express = require ('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
-require('dotenv').config()
 
 const app = express();
-// middlewere
+
+// middleware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tw8naco.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {serverApi: {version: ServerApiVersion.v1,strict: true, deprecationErrors: true}});
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, { serverApi: {version: ServerApiVersion.v1,strict: true, deprecationErrors: true,}});
+console.log(uri)
+async function run() {
+    try {
+        const productCollection = client.db('borolokerGadget').collection('products');
+       
 
-async function run(){
-    try{
+        // Use Aggregate to query multiple collection and then merge data
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const options = await productCollection.find(query).toArray();
+
+            res.send(options);
+            
+        });
+
+       
+
+       
 
     }
-    finally{
+    finally {
 
     }
 }
-run().catch(console.log)
+run().catch(console.log);
 
-app.get('/', async(req,res)=>{
-    res.send('boroloker gadGet Server is running!!!!!')
-
+app.get('/', async (req, res) => {
+    res.send('borolokerGadget server is running');
 })
 
-app.listen(port, ()=> console.log(`boroloker-server site runing on ${port}`))
+app.listen(port, () => console.log(`BorolokerGadget running on ${port}`))
